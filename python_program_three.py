bc7ec0fe587e96747d27e4830f48bdb4b34ffe57
kalc1 <kalcocer1@gmail.com>
2023-09-13 16:36:11 -0700

    Define 'name', 'phone number', and 'email'

--- python_program_three.py
+++ python_program_three.py
@@ -8,3 +8,11 @@
 # Example of a contact: Kevin {name : Kevin, phone : 714714, email: 714@scccd.edu} <--- this data values need to be input by users
 
 Contacts = []
+
+# "Your program should allow users to add, view, and search for contacts." <- This part of the Assignment Details tells me that the input() function needs to be used in order to add a new contact, phone number, and email:
+
+name = Input("Please enter the name of your contact: ")
+phone = Input("Please enter the name of your contact: ")
+email = Input("Please enter the name of your contact: ")
+
+# Now I have to add these values to the empty Contacts list. 
